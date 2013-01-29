0f2c97cd6aa6295b6889940397325c37e9b15aca
Ozan Turgut <ozanturgut@gmail.com>
2013-01-28 23:58:41 -0800

    Updates for the Thrill 0.4 API
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- tasks/thrill.js
+++ tasks/thrill.js
@@ -5,14 +5,23 @@ module.exports = function(grunt){
 		var config = this.data,
 			done = this.async();
 		
-		if(config.run) config.run = grunt.file.expand(config.run);
-		
+		if(config.run){
+			config.run = grunt.file.expand(config.run);	
+			if(config.run.length === 1) config.run = config.run[0];
+		}
+
 		if(typeof config === "string") config = {file: config};
-		runner(config, function(test){
-			if(test.passed){
+
+		runner(config, function(passed){
+			if(passed instanceof Error){
+				grunt.fatal('Test did not pass. An unknown error occurred');
+				done(false);
+				return;
+			}
+			
+			if(passed){
 				done(true);
 			} else {
-				grunt.fatal('Test did not pass.');
 				done(false);
 			}
 		});
